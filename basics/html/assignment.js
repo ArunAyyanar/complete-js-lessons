48282b68f08fe2df81161828e44ca068260a695e
Arun Ayyanar <arunctech89@gmail.com>
2023-10-24 18:17:14 -0400

    Datatypes

---
+++ basics/html/assignment.js
@@ -0,0 +1,11 @@
+let country = "India";
+let continent = "Asia";
+let population = 1423;
+let isIsland = false;
+let language;
+
+console.log("My Country is", country);
+console.log(country, "is in", continent, "Continent");
+console.log("Total population of", country, "is", population, "millions");
+console.log("isIsland", isIsland);
+console.log("language", language);
---
+++ basics/html/script.js
@@ -0,0 +1,34 @@
+// Variables and values
+
+let $firstName = "Arun";
+let _lastName = "Ayyanar";
+console.log($firstName + " " + _lastName);
+
+let name;
+console.log(name);
+//console.log(name === typeof name);
+
+const PI = 3.14;
+console.log(PI);
+
+// Data types - Number,String,Boolean,undefined,Null,Symbol,BigInt
+
+name = "Arun";
+console.log(name);
+
+let age = 34;
+console.log(age);
+
+let isAlive = true;
+console.log(isAlive);
+
+let employee = {};
+employee.name = "Arun";
+employee.age = 34;
+console.log(employee.name);
+console.log(employee["age"]);
+
+let marks = [];
+marks.push("arun");
+marks.push(34);
+console.log(marks);
