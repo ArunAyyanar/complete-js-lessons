c1e4a444284dcf33d0a39c73259d8877485a4296
Arun Ayyanar <arunctech89@gmail.com>
2023-10-24 22:30:49 -0400

    operators demo

---
+++ basics/html/letDemo.js
@@ -0,0 +1,13 @@
+// var - Function level scope
+// let - Block level scope
+// const - To store constant values
+const PI = () => 3.14;
+if (true) {
+  // let name = "balaji";
+  var name = "arun";
+}
+console.log(name);
+console.log(PI());
+
+firstName = "arun";
+console.log(firstname);
