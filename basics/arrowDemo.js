9f1aae2beb50b7e3b15017a7cec1b4fd451c89d0
Arun Ayyanar <arunctech89@gmail.com>
2023-10-26 21:47:14 -0400

    functions

--- basics/arrowDemo.js
+++ basics/arrowDemo.js
@@ -3,3 +3,7 @@ let sum = (num1, num2) => num1 + num2;
 console.log(sum(12, 56));
 
 console.log(sum(9, 22));
+
+const display = (msg) => console.log(msg);
+
+display("Hello");
---
+++ basics/html/strictDemo.js
@@ -0,0 +1,8 @@
+//"use strict";
+const private1 = "using private keyword";
+console.log(private1);
+
+const hasEnoughMoney = true;
+if (hasEnoughMoneys) {
+  console.log("have enough money :)");
+}
