186b609de4eed917642a4cdf326c5b4b74aaec71
Arun Ayyanar <arunctech89@gmail.com>
2023-11-14 22:41:13 -0500

    Array+object

---
+++ basics/ArrayVsObject.js
@@ -0,0 +1,17 @@
+const arunArr = [
+  "arun",
+  "ayyanar",
+  2023 - 1989,
+  ["prabha", "arunkumar", "siva"],
+];
+
+console.log(arunArr);
+
+const arunObj = {
+  firstName: "arun",
+  lastName: "ayyanar",
+  age: 2023 - 1989,
+  friends: ["prabha", "arunkumar", "siva"],
+};
+
+console.log(arunObj);
